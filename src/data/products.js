479199914af39613a3e0560cda1107a353fa0d20
BenjapolDissaro <139202610+BenjapolDissaro@users.noreply.github.com>
2023-08-02 11:56:35 +0700

    Create Shopping App

---
+++ src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id:1,
+        name : "หมวก",
+        image:"assets/product1.png",
+        price:2000,
+        quantity:12
+    },
+    {
+        id:2,
+        name : "หูฟัง",
+        image:"assets/product2.jpg",
+        price:1500,
+        quantity:25
+    },
+    {
+        id:3,
+        name : "กระเป๋า",
+        image:"assets/product3.jpeg",
+        price:1000,
+        quantity:13
+    }
+]
+
+export default products
